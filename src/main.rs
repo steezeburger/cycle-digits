1ad65682fd9915b554a98dde7a57903fff184fc7
Jesse Snyder <jessetsnyder@gmail.com>
2023-11-03 23:30:26 -0600

    cycle the digits!

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@
 
 use bsp::entry;
 use defmt::*;
-use defmt_rtt as _;
-use embedded_hal::digital::v2::{ InputPin, OutputPin };
-use panic_probe as _;
+// use embedded_hal::digital::v2::{InputPin, OutputPin};
+use smart_leds::{SmartLedsWrite, RGB8};
+use ws2812_pio::Ws2812;
 
 use adafruit_macropad as bsp;
 
@@ -13,8 +13,47 @@ use bsp::hal::{
     clocks::{init_clocks_and_plls, Clock},
     pac,
     sio::Sio,
+    pio::PIOExt,
     watchdog::Watchdog,
+    Timer,
 };
+
+const RED: RGB8 = RGB8::new(255, 0, 0);
+const GREEN: RGB8 = RGB8::new(0, 255, 0);
+const BLUE: RGB8 = RGB8::new(0, 0, 255);
+const WHITE: RGB8 = RGB8::new(255, 255, 255);
+/// Map the LED index to the 2D grid coordinates
+const LED_MAP: [&'static [(u8, u8)]; 10] = [
+    &[(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 2), (3, 0), (3, 1), (3, 2)], // 0
+    &[(0, 1), (1, 0), (1, 1), (2, 1), (3, 0), (3, 1), (3, 2)],                         // 1
+    &[(0, 0), (0, 1), (0, 2), (1, 2), (2, 0), (2, 1), (3, 0), (3, 1), (3, 2)],         // 2
+    &[(0, 0), (0, 1), (0, 2), (1, 1), (1, 2), (2, 2), (3, 0), (3, 1), (3, 2)],         // 3
+    &[(0, 0), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2), (3, 2)],                 // 4
+    &[(0, 0), (0, 1), (0, 2), (1, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2)],         // 5
+    &[(0, 0), (0, 1), (0, 2), (1, 0), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2)], // 6
+    &[(0, 0), (0, 1), (0, 2), (1, 2), (2, 2), (3, 2)],                                 // 7
+    &[(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2), (3, 0), (3, 1), (3, 2)], // 8
+    &[(0, 0), (0, 1), (0, 2), (1, 0), (1, 2), (2, 0), (2, 1), (2, 2), (3, 2)]          // 9
+];
+
+type LedStates = [RGB8; 12];
+
+/// Get the LED states for a given digit
+fn get_led_states(digit: u8) -> LedStates {
+    let mut led_states = [RGB8::default(); 12];
+
+    for &(x, y) in LED_MAP[digit as usize] {
+        let index = map_coord_to_index(x, y);
+        led_states[index as usize] = RED;
+    }
+
+    led_states
+}
+
+/// Map the 2D grid coordinates to the LED index
+fn map_coord_to_index(x: u8, y: u8) -> u8 {
+    x * 3 + y
+}
 
 #[entry]
 fn main() -> ! {
@@ -35,8 +74,8 @@ fn main() -> ! {
         &mut pac.RESETS,
         &mut watchdog,
     )
-    .ok()
-    .unwrap();
+        .ok()
+        .unwrap();
 
     let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
 
@@ -47,22 +86,23 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // the first key on the macropad
-    let key_1_button = pins.key1.into_pull_up_input();
-
-    // the little red led near the usb port on the macropad
-    let mut led_pin = pins.led.into_push_pull_output();
-
-    // start with the led off
-    led_pin.set_low().unwrap();
+    let timer = Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
+    // Setup PIO
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+    // Setup Neopixel RGB LED
+    let mut ws = Ws2812::new(
+        pins.neopixel.into_function(),
+        &mut pio,
+        sm0,
+        clocks.peripheral_clock.freq(),
+        timer.count_down(),
+    );
 
     loop {
-        if key_1_button.is_low().unwrap() {
-            info!("on!");
-            led_pin.set_high().unwrap();
-        } else {
-            info!("off!");
-            led_pin.set_low().unwrap();
+        for digit in 0..=9 {
+            let led_states = get_led_states(digit as u8);
+            ws.write(led_states.iter().copied()).unwrap();
+            delay.delay_ms(500);
         }
     }
 }
